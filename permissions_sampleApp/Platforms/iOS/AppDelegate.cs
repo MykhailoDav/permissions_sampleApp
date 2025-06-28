eea511ce06ba468010534d68a6b8d836b86c460c
Mykhailo Davydenko <davudenko.mukhailo@outlook.com>
2025-06-29 01:28:34 +0300

    removed platforms

--- permissions_sampleApp/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace permissions_sampleApp;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
